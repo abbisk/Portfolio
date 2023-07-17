bd0a895b54642c523a166dd7f015cc85c2db58fd
abbisk <abbiskpro@gmail.com>
2023-07-17 12:31:55 +0530

    Hero section added

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+// components/HeroSection.js (updated)
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-content">
+        <h1>Welcome to My Portfolio</h1>
+        <p>I am a passionate web developer.</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
