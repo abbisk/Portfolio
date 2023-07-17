060faba1aa90d87f61f4fe8b6bab49a89c5262aa
abbisk <abbiskpro@gmail.com>
2023-07-17 12:31:02 +0530

    About section added

---
+++ src/components/AboutSection.js
@@ -0,0 +1,17 @@
+// components/AboutSection.js (updated)
+import React from 'react';
+
+const AboutSection = () => {
+  return (
+    <section className="about" id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a web developer with experience in React.js and CSS. I love
+        creating beautiful and responsive websites that provide a great user
+        experience.
+      </p>
+    </section>
+  );
+};
+
+export default AboutSection;
